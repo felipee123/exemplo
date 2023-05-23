cb77c3dc69c00c91a72038f769bbe7603480577f
FELIPE LEITE 34 <felipeleitedossantos123@gmail.com>
2023-05-22 21:07:30 -0300

    Salvar a modificação

--- ---celular/src/Screens/Tela2/index.tsx
+++ ---celular/src/Screens/Tela2/index.tsx
@@ -1,13 +1,31 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet, Button } from 'react-native';
+;
 
-export interface Tela2ScreenProps{
+const styles = StyleSheet.create({
+  tela2ScreenProps: {
+    flexDirection: 'row',
+  },
+  imagem: { 
+    width: 150, 
+    height: 250 
+  }
+});
+
+export interface Tela2ScreenProps {
 }
 
+
 export function Tela2Screen(props: Tela2ScreenProps) {
-    return (
-      <View>
-         <Text>Appgfhjiofhg</Text>
-      </View>
-    );
+  return (
+    <View style={{justifyContent:'center', flex:1 , alignItems:'center' }}>
+      <Text>Qual o Melhor Personagem</Text>
+      <View style={styles.tela2ScreenProps}>
+        <Image style={styles.imagem} source={require("../../img/capiao_america.jpg")} />
+        <Image style={styles.imagem} source={require("../../img/batman.webp")} />
+                 </View>
+                  <Button title="capiao America 1"  color="#841584" />
+                  <Button title="The Batman 2"  color="#841584" />
+    </View>
+  );
 }
